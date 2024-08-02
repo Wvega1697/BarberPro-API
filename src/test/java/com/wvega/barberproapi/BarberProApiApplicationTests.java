118ce8424c2e6c8eaefb8ab4e404c83ab126fe6c
WVEGA1697 <williamsv1697@gmail.com>
2024-08-01 22:26:01 -0600

    INIT
    Initial Commit

---
+++ src/test/java/com/wvega/barberproapi/BarberProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvega.barberproapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberProApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

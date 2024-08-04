7f5e4a8e7d4d799cd16d887317bd59afe1463fcf
WVEGA1697 <williamsv1697@gmail.com>
2024-08-03 19:19:18 -0600

    ADD
    Functional stats integration.

---
+++ src/main/java/com/wvega/barberproapi/controller/StatsController.java
@@ -0,0 +1,57 @@
+package com.wvega.barberproapi.controller;
+
+import com.wvega.barberproapi.service.StatsService;
+import com.wvega.barberproapi.utils.ResponseWS;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import static com.wvega.barberproapi.utils.Constants.*;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/stats")
+public class StatsController {
+
+    private final StatsService statsService;
+
+    @GetMapping
+    public ResponseWS getStats(@RequestParam(required = false) Integer page,
+                               @RequestParam(required = false) Integer size,
+                               @RequestParam(required = false) String orderBy) {
+        return statsService.getStats(page, size, orderBy);
+    }
+
+    @GetMapping("/process/{id}")
+    public ResponseWS getStatsByProcessId(@PathVariable String id) {
+        return statsService.getStatsByField(PROCESSID, id);
+    }
+
+    @GetMapping("/method/{name}")
+    public ResponseWS getStatsByMethodName(@PathVariable String name) {
+        return statsService.getStatsByField(METHOD_NAME, name);
+    }
+
+    @GetMapping("/status/{type}")
+    public ResponseWS getStatsByStatus(@PathVariable String type) {
+        return statsService.getStatsByField(STATUS, type);
+    }
+
+    @GetMapping("/status")
+    public ResponseWS getStatsGroupedByStatus() {
+        return statsService.getStatsGroupedByStatus();
+    }
+
+    @GetMapping("/categories")
+    public ResponseWS getProductCountsByCategory() {
+        return statsService.getProductCountsByCategory();
+    }
+
+    @GetMapping("/averages")
+    public ResponseWS getAverageExecutionTimeByMethod() {
+        return statsService.getAverageExecutionTimeByMethod();
+    }
+}
+
+
--- src/main/java/com/wvega/barberproapi/utils/Constants.java
+++ src/main/java/com/wvega/barberproapi/utils/Constants.java
@@ -1,15 +1,52 @@
 package com.wvega.barberproapi.utils;
 
+import java.time.ZoneOffset;
+
 public class Constants {
 
-    //String constants
+    private Constants() {
+    }
+
+    //Product strings
     public static final String NAME = "name";
     public static final String PRICE = "price";
     public static final String DURATION_MINUTES = "durationMinutes";
     public static final String CATEGORY = "category";
     public static final String DESCRIPTION = "description";
 
+    //Stat strings
+    public static final String METHOD_NAME = "methodName";
+    public static final String EXECUTION_TIME = "executionTime";
+    public static final String PROCESSID = "processId";
+    public static final String DATEMILLIS = "dateMillis";
+    public static final String STATUS = "status";
+    public static final String DEFAULT_BULK_ID = "SEARCH_ALL";
+    public static final String DEFAULT_PROCESSID = "ERROR";
+
     //Firebase
     public static final String PRODUCTS_COLLECTION = "Products";
+    public static final String STATS_COLLECTION = "Stats";
     public static final int DEFAULT_SIZE = 20;
+
+    //Dates
+    public static final ZoneOffset UTC_MINUS_4 = ZoneOffset.of("-04:00");
+
+    //Log
+    public static final String STATS_SUCCESS = "Stats retrieved successfully";
+    public static final String STATS_FAIL = "No stats retrieved";
+    public static final String STATS_ERROR = "Error retrieving stats: ";
+
+    //Methods Name
+    public static final String METHOD_ADD = "createProduct";
+    public static final String METHOD_UPDATE = "updateProduct";
+    public static final String METHOD_DELETE = "deleteProduct";
+    public static final String METHOD_SEARCH_ALL = "getAllProducts";
+    public static final String METHOD_SEARCH_ID = "getProductById";
+    public static final String METHOD_SEARCH_FIELDS = "getProductsByFields";
+
+    //Status
+    public static final String STATUS_SUCCESS = "SUCCESS";
+    public static final String STATUS_FAIL = "FAIL";
+    public static final String STATUS_ERROR = "ERROR";
+
 }
